30ac99d6a93b875c55ccd16fe59e071bdac34cad
avvppro <avvppro@gmail.com>
2020-09-02 23:06:15 +0300

    Jenkins pipe

---
+++ Backend.groovy
@@ -0,0 +1,58 @@
+#!groovy
+properties([disableConcurrentBuilds()])
+
+pipeline {
+    environment { 
+        registry = "avvppro/dtester" 
+        registryCredential = 'dockerhub_id' 
+        dockerImage = '' 
+    }
+    agent any
+    stages {
+        stage("Make Backend Files") {
+            steps {
+                sh 'git clone https://github.com/koseven/koseven'
+                sh 'mkdir ./application/logs ./application/cache'
+                sh 'chmod 766 ./application/logs'
+                sh 'chmod 766 ./application/cache'
+                sh 'mv ./koseven/public/index.php ./'
+                sh 'mv ./koseven/modules/ ./'
+                sh 'mv ./koseven/system/ ./'
+            }
+        }
+        stage("Set variables") {
+            steps {
+                load "$JENKINS_HOME/dtvariables.groovy"
+                sh "sed -i 's|RewriteBase /|RewriteBase /dtapi/|g' ./.htaccess"
+                sh "sed -i 's|'base_url'   => '/'|'base_url'   => '/dtapi/'|g' ./application/bootstrap.php"
+                sh "sed -i 's/PDO_MySQL/PDO/g' ./application/config/database.php"
+                sh "sed -i 's/mysql:host=localhost/mysql:host=${env.DB_HOST}/g' ./application/config/database.php"
+                sh "sed -i 's/'username'   => 'dtapi'/'username'   => '${env.DB_USER}'/g' ./application/config/database.php"
+                sh "sed -i 's/'password'   => 'dtapi'/'password'   => '${env.DB_PASS}'/g' ./application/config/database.php"
+                sh "rm -rf koseven/ Dockerfile Backend.groovy dtapi.sql README.md .git .gitignore"
+            }
+        }
+        stage("Build Docker Image") {
+            steps {
+                script { 
+                    dockerImage = docker.build registry + ":backend$BUILD_NUMBER" 
+                }
+            }
+        }
+        stage('Deploy our image') { 
+            steps { 
+                script { 
+                    docker.withRegistry( '', registryCredential ) { 
+                        dockerImage.push() 
+                    }
+                } 
+            }
+        } 
+        stage('Cleaning up') { 
+            steps { 
+                sh "docker rmi $registry:backend$BUILD_NUMBER"
+                sh 'rm -rf ./*'
+            }
+        } 
+    }
+}
---
+++ Dockerfile
@@ -0,0 +1,12 @@
+
+FROM alpine:3.12.0
+RUN apk update && apk --no-cache upgrade
+RUN apk add --no-cache openrc apache2 php7-apache2 libmcrypt-dev php7 php7-fpm php7-opcache php7-pdo_mysql php7-xml \
+    php7-xmlrpc php7-json php7-soap php7-mbstring php7-pecl-mcrypt php7-pecl-memcache php7-mysqli php7-ctype
+RUN rc-update add php-fpm7 default
+RUN sed -i '265s/AllowOverride none/AllowOverride All/g' /etc/apache2/httpd.conf 
+RUN sed -i "s%#LoadModule rewrite_module%LoadModule rewrite_module%g" /etc/apache2/httpd.conf
+COPY dtapi/ /var/www/localhost/htdocs/dtapi
+RUN chown apache. -R /var/www/localhost/htdocs/
+CMD ["-D","FOREGROUND"]
+ENTRYPOINT ["/usr/sbin/httpd"]
